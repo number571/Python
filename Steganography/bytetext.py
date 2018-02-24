1d6c8bd916a2bdbf6d0850bcaf54efef6556fc9e
[#571] <31502293+Number571@users.noreply.github.com>
2018-02-25 03:41:00 +0700

    Update and rename bytetext.py to writeBytes.py

--- Steganography/bytetext.py
+++
@@ -1,3 +0,0 @@
-with open(input("File-Cover: "), 'ab') as file: 
-	file.write(input("Write the text: ").encode("utf-8")) 
-	print("[+] File successfully overwritten!")
---
+++ Steganography/writeBytes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+# chmod +x main.py
+# ./main.py file.jpg
+
+try:
+	nameFile = argv[1]
+	textFile = argv[2:]
+except:
+	print("Error: Arguments!"); raise SystemExit
+	
+with open(nameFile, 'ab') as file: 
+	file.write("".join(textFile).encode("utf-8")) 
+	print("[+] File successfully overwritten!")
