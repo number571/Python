20257d9277ed9cd96692fb804c595ef4ee0ab173
[#571] <31502293+Number571@users.noreply.github.com>
2018-02-25 03:33:44 +0700

    Update and rename readbyte.py to readBytes.py

---
+++ Steganography/readBytes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+# chmod +x main.py
+# ./main.py file.jpg
+
+try:
+	nameFile = argv[1]
+except:
+	print("Error: arguments"); raise SystemExit
+	
+try:
+	with open(nameFile, "rb") as file:
+		counter = 0 
+		while byte:
+			byte = file.read(1)
+			print(byte)
+			counter += 1
+except FileNotFoundError: 
+	print("[x] File: '{name}' is not defined!".format(name = nameFile))
+else:
+	print("Number of bytes in the '{name}': {number}".format(name = nameFile, number = counter))
