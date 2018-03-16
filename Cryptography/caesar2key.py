cee6d96fedaaeae2840710d6ee4ed69717c82ea2
[#571] <31502293+Number571@users.noreply.github.com>
2018-03-16 08:01:16 +0700

    Update caesar2key.py

--- Cryptography/caesar2key.py
+++ Cryptography/caesar2key.py
@@ -1,22 +1,35 @@
 cryptMode = input("[E]ncrypt|[D]ecrypt: ").upper()
 if cryptMode not in ['E','D']:
     print("Error: mode is not Found!"); raise SystemExit
+
+alphaList = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 startMessage = input("Write the message: ").upper()
-try:numberKey = int(input("Write the number key: "))
+
+try: numberKey = int(input("Write the number key: "))
 except ValueError: print("Only numbers!"); raise SystemExit
-alpha = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
 stringKey = list(input("Write the string key: ").upper())
-for symbol in stringKey:
-    if symbol not in [chr(x) for x in range(65,91)] \
-    or stringKey.count(symbol) > 1: stringKey.remove(symbol)
-    if symbol in alpha: alpha.remove(symbol)
-for index, symbol in enumerate(stringKey):
-    alpha.insert((numberKey+index)%26, symbol)
+
+def remove(alpha, string):
+    for symbol in string:
+        if symbol not in [chr(x) for x in range(65,91)] \
+        or string.count(symbol) > 1: string.remove(symbol) 
+        if symbol in alpha: alpha.remove(symbol)
+    return alpha, string
+
+def insert(alpha_string):
+    for index, symbol in enumerate(alpha_string[1]):
+        alpha_string[0].insert((numberKey+index)%26, symbol)
+    return alpha_string[0]
+
 def encryptDecrypt(mode, message, key, final = ""):
+    alpha = insert(remove(alphaList, stringKey))
     for symbol in message:
         if mode == 'E':
             final += alpha[(alpha.index(symbol) + key)%26]
         else: 
             final += alpha[(alpha.index(symbol) - key)%26]
     return final
-print("Final message:", encryptDecrypt(cryptMode, startMessage, numberKey))
+
+message = encryptDecrypt(cryptMode, startMessage, numberKey)
+print("Final message:", message)
