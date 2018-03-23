80b05bf765146509c38d4944fe790b3e475727c6
[#571] <31502293+Number571@users.noreply.github.com>
2018-03-24 06:35:08 +0700

    Update calculator.py

--- GUI_Scripts/calculator.py
+++ GUI_Scripts/calculator.py
@@ -16,11 +16,14 @@ class CalculatorApp(App):
 
     def calculate(self, symbol):
         global saveInput
-        if symbol.text is not '=':
+        if symbol.text == '<':
+            saveInput = self.result.text = ""
+        elif symbol.text != '=':
             self.result.text += symbol.text
             saveInput += symbol.text
         else:
-            saveInput = self.result.text = str(eval(saveInput))
+            try: saveInput = self.result.text = str(eval(saveInput))
+            except: saveInput = self.result.text = ""
         
     def build(self):
         root = BoxLayout(orientation = "vertical", padding = 5)
@@ -28,27 +31,31 @@ class CalculatorApp(App):
         self.result = TextInput(text = "", readonly = True, font_size = 25, size_hint = [1,.7])
         root.add_widget(self.result)
 
-        allButtons = GridLayout(cols = 4)
+        allButtons = GridLayout(cols = 5)
 
         allButtons.add_widget(Button(text = '7', on_press = self.calculate))
         allButtons.add_widget(Button(text = '8', on_press = self.calculate))
         allButtons.add_widget(Button(text = '9', on_press = self.calculate))
         allButtons.add_widget(Button(text = "+", on_press = self.calculate))
+        allButtons.add_widget(Button(text = "<", on_press = self.calculate))
 
         allButtons.add_widget(Button(text = '4', on_press = self.calculate))
         allButtons.add_widget(Button(text = '5', on_press = self.calculate))
         allButtons.add_widget(Button(text = '6', on_press = self.calculate))
         allButtons.add_widget(Button(text = "-", on_press = self.calculate))
+        allButtons.add_widget(Button(text = "(", on_press = self.calculate))
 
         allButtons.add_widget(Button(text = '1', on_press = self.calculate))
         allButtons.add_widget(Button(text = '2', on_press = self.calculate))
         allButtons.add_widget(Button(text = '3', on_press = self.calculate))
         allButtons.add_widget(Button(text = "*", on_press = self.calculate))
+        allButtons.add_widget(Button(text = ")", on_press = self.calculate))
 
         allButtons.add_widget(Button(text = '0', on_press = self.calculate))
         allButtons.add_widget(Button(text = ".", on_press = self.calculate))
         allButtons.add_widget(Button(text = "=", on_press = self.calculate))
         allButtons.add_widget(Button(text = "/", on_press = self.calculate))
+        allButtons.add_widget(Button(text = "%", on_press = self.calculate))
 
         root.add_widget(allButtons)
         return root
