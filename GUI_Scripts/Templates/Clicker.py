d704cc141f8fbe768c6761e82162c6fc1929e52f
[#571] <31502293+Number571@users.noreply.github.com>
2018-03-24 11:12:22 +0700

    Update Clicker.py

--- GUI_Scripts/Templates/Clicker.py
+++ GUI_Scripts/Templates/Clicker.py
@@ -1,5 +1,11 @@
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.config import Config
+
+Config.set("graphics", "resizable", "0")
+Config.set("graphics", "width", "500")
+Config.set("graphics", "height", "400")
+
 from random import random
 
 class ClickerApp(App):
