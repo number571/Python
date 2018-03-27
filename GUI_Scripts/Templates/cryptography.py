79a1895b2dca0045901992454765829b5cd9cec6
[#571] <31502293+Number571@users.noreply.github.com>
2018-03-27 07:16:17 +0700

    Update cryptography.py

--- GUI_Scripts/Templates/cryptography.py
+++ GUI_Scripts/Templates/cryptography.py
@@ -361,8 +361,113 @@ def fence(mode, message, final = ""):
     return final
 
 ### Great Cipher ###
-def greatcipher(mode, message):
-    pass
+def greatcipher_regular(text):
+        template = r"[0-9]{3}"
+        return findall(template, text)
+def greatcipher(mode, message, string = "", final = ""):
+    message = message.upper()
+    from memory import Key, Limit
+    keysCrypt = {
+        'A':Key[0:8],     'B':Key[8:10],
+        'C':Key[10:13],   'D':Key[13:17],
+        'E':Key[17:29],   'F':Key[29:31],
+        'G':Key[31:33],   'H':Key[33:39],
+        'I':Key[39:45],   'J':[Key[45]],
+        'K':[Key[46]],    'L':Key[47:51],
+        'M':Key[51:53],   'N':Key[53:59],
+        'O':Key[59:66],   'P':Key[66:68],
+        'Q':[Key[68]],    'R':Key[69:75],
+        'S':Key[75:81],   'T':Key[81:90],
+        'U':Key[90:93],   'V':[Key[93]],
+        'W':Key[94:96],   'X':[Key[96]],
+        'Y':Key[97:99],   'Z':[Key[99]],
+        ' ':Key[100:118]
+    }
+    listWord = ('TO','WHY','WITH','WAR','NOT','IN','OR','ELSE','THE','THAT','BY',
+    'AND','HOW','BUT','IF','ONE','YOU','ME','USE','HIS','YOUR','ON','OF','WAS','BE',
+    'THIS','WHAT','THEY','NO','YES','TRUE','FALSE','CALL','FEEL','CLOSE','VERY',
+    'WHICH','CAR','ANY','HOLD','WORK','RUN','NEVER','START','EVEN','LIGHT','THAN',
+    'AFTER','PUT','STOP','OLD','WATCH','FIRST','MAY','TALK','ANOTHER','BEHIND',
+    'CUT','MEAN','SMILE','OUR','MUCH','IT','HE','SHE','ITS','HOUSE','KEEP','YEAH',
+    'PLACE','BEGIN','NOTHING','YEAR','MAN','WOMAN','BECAUSE','THREE','SEEM','ARE',
+    'WAIT','NEED','LAST','LATE','SURE','BIG','SMALL','FRONT','REALLY','NAME','ALL',
+    'NEW','GUY','ANYTHING','SHOULD','KILL','POINT','WALL','BLACK','STEP','SECOND',
+    'LIFE','MAYBE','FALL','OWN','FAR','WHILE','FOR','HELP','END','THOSE','SAME',
+    'REACH','GIRL','STREET','NEXT','FEW','FEET','SHOW','MUST','TABLE','OK','IS',
+    'OKAY','BODY','PHONE','ADD','WATER','FIRE','INSIDE','BREAK','EVER','SHAKE',
+    'MEET','GREAT','MIND','ENOUGH','MINUTE','FOLLOW','ATTACK','DEAD','ALMOST')
+    position = 118
+    keysCode = {listWord[x]:Key[x + position] for x in range(len(listWord))}
+    listSyllables = ('TH','WH','EE','AI','OO','IS','ING','ED','BE','ON','OR','ER',
+    'CH','SH','GH','EN','EA','OU','LL','US','SE','AL','ST','EV','WO','UI','IN','RE',
+    '!','?','.',',','@','#','$','%','*','^','-','+','=','/',':',';','&','~')
+    position = len(listWord) + 118
+    keysSyllables = {listSyllables[x]:Key[x + position] for x in range(len(listSyllables))}
+    listSpecial = ('<-','->','<+','+>')
+    position = len(listWord) + len(listSyllables) + 118
+    keysSpecial = {listSpecial[x]:Key[x + position] for x in range(len(listSpecial))}
+    position = len(listWord) + len(listSyllables) + len(listSpecial) + 118
+    traps = tuple([Key[x] for x in range(position, Limit)])
+    del listWord, listSpecial, position
+    if mode == 'E':
+        secondText = findall(r"[^\s]+", message)
+        del message
+        for indexWord in range(len(secondText)):
+            if secondText[indexWord] in keysSpecial:
+                secondText[indexWord] = keysSpecial[secondText[indexWord]]
+        for indexWord in range(len(secondText)):
+            if secondText[indexWord] in keysCode:
+                secondText[indexWord] = keysCode[secondText[indexWord]]
+        for indexWord in range(len(secondText)):
+            for syllable in keysSyllables:
+                if syllable in secondText[indexWord]:
+                    secondText[indexWord] = secondText[indexWord].replace(syllable,keysSyllables[syllable])
+        for indexWord in range(len(secondText)):
+            secondText[indexWord] = list(secondText[indexWord])
+        for indexWord in range(len(secondText)):
+            secondText[indexWord].append(' ')
+        for indexWord in range(len(secondText)):
+            for indexSymbol in range(len(secondText[indexWord])):
+                symbol = secondText[indexWord][indexSymbol]
+                if symbol in keysCrypt:
+                    length = len(keysCrypt[symbol])
+                    secondText[indexWord][indexSymbol] = keysCrypt[symbol][randint(0, length - 1)]
+        for word in secondText:
+            string += "".join(word)
+        finalList = list(greatcipher_regular(string))
+        for indexList in range(len(finalList)):
+            randSwitch = randint(0,2); randPosition = randint(0,len(finalList))
+            if not randSwitch: finalList.insert(randPosition,choice(traps))
+        for word in finalList:
+            final += "".join(word)
+        return ".".join(greatcipher_regular(final))
+    else:
+        for symbolText in greatcipher_regular(message):
+            for element in keysSpecial: # Перебор всех специальных символов
+                if symbolText == keysSpecial[element]: final += element
+            for word in keysCode: # Перебор всех кодов
+                if symbolText == keysCode[word]: final += word
+            for syllable in keysSyllables: # Перебор всех слогов
+                if symbolText == keysSyllables[syllable]: final += syllable
+            for symbol in keysCrypt: # Перебор всех шифров
+                if symbolText in keysCrypt[symbol]: final += symbol
+        listWord = findall(r"[^\s]+",final)
+        for _ in range(len(listWord)):
+            for element in keysSpecial:
+                if element in listWord:
+                    if element == '<-':
+                        del listWord[listWord.index(element) - 1]
+                        listWord.remove(element)
+                    elif element == '->':
+                        del listWord[listWord.index(element) + 1]
+                        listWord.remove(element)
+                    elif element == '<+':
+                        listWord[listWord.index(element)] = listWord[listWord.index(element) - 1]
+                    elif element == '+>':
+                        listWord[listWord.index(element)] = listWord[listWord.index(element) + 1]
+                    else: pass
+        final = " ".join(listWord)
+    return final
 
 ### Gronsfeld ###
 def gronsfeld(mode, message, key, final = ""):
