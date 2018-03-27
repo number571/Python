f12512b32a84d422ba336ae3c0b6192eff1e8d98
[#571] <31502293+Number571@users.noreply.github.com>
2018-03-27 07:02:03 +0700

    Update cryptography.py

--- GUI_Scripts/Templates/cryptography.py
+++ GUI_Scripts/Templates/cryptography.py
@@ -18,7 +18,9 @@ from os import remove
 from re import findall
 from os.path import splitext
 from random import choice, randint
+
 from pyAesCrypt import encryptFile, decryptFile
+from rsa import newkeys, encrypt, PublicKey, decrypt, PrivateKey
 
 def getNumbers(text):
     template = r"[0-9]+"
@@ -162,7 +164,6 @@ def aes(mode, file, password, final = ""):
         except FileNotFoundError: return ":: File not found."
         except ValueError: return ":: Password is False."
         else: return ":: File '{name}' overwritten.".format(name = str(file))
-
 
 ### Affine ###
 def affine(mode, message, key, final = ""):
@@ -399,7 +400,8 @@ def hill2x2(mode, message, matrix):
     else: return hill2x2_crypt(getTwoSymbols(message), imatrix)
 
 ### Hill[3x3] ###
-# pass
+def hill3x3(mode, message, key):
+    pass
 
 ### Homophonic ###
 def homophonic(mode, message, final = ""):
@@ -435,7 +437,354 @@ def homophonic(mode, message, final = ""):
     return final
 
 ### Lattice ###
+def lattice(message):
+    pass
 
+### Playfair ###
+def playfair(mode, message, final = ""):
+    matrix = [
+        ['S','O','M','E','T'],
+        ['H','I','N','G','A'],
+        ['B','C','D','F','K'],
+        ['L','P','Q','R','U'],
+        ['V','W','X','Y','Z']
+    ]; addSymbol = 'X'
+    message = list(message.upper())
+    if mode == 'E':
+        for symbol in message:
+            if symbol not in [chr(x) for x in range(65,91)]:
+                message.remove(symbol)
+        for index in range(len(message)):
+            if message[index] == 'J': message[index] = 'I'
+        for index in range(1,len(message)):
+            if message[index] == message[index - 1]:
+                message.insert(index,addSymbol)
+        if len(message) % 2 != 0:
+            message.append(addSymbol)
+    binaryList = getTwoSymbols("".join(message))
+    for binary in range(len(binaryList)):
+        binaryList[binary] = list(binaryList[binary])
+        for indexString in range(len(matrix)):
+            for indexSymbol in range(len(matrix[indexString])):
+                if binaryList[binary][0] == matrix[indexString][indexSymbol]:
+                    y0, x0 = indexString, indexSymbol
+                if binaryList[binary][1] == matrix[indexString][indexSymbol]:
+                    y1, x1 = indexString, indexSymbol
+        for indexString in range(len(matrix)):
+            if matrix[y0][x0] in matrix[indexString] and matrix[y1][x1] in matrix[indexString]:
+                if mode == 'E':
+                    x0 = x0 + 1 if x0 != 4 else 0
+                    x1 = x1 + 1 if x1 != 4 else 0
+                else:
+                    x0 = x0 - 1 if x0 != 0 else 4
+                    x1 = x1 - 1 if x1 != 0 else 4
+        y0,y1 = y1,y0
+        binaryList[binary][0] = matrix[y0][x0]
+        binaryList[binary][1] = matrix[y1][x1]
+    for binary in range(len(binaryList)):
+        for symbol in binaryList[binary]:
+            final += symbol
+    return final
+
+### Polibiy ###
+def polibiy(mode, message):
+    final = []; message = message.upper()
+    keys = {
+        'A':'11', 'B':'12', 'C':'13', 'D':'14',
+        'E':'15', 'F':'16', 'G':'21', 'H':'22',
+        'I':'23', 'J':'24', 'K':'25', 'L':'26',
+        'M':'31', 'N':'32', 'O':'33', 'P':'34',
+        'Q':'35', 'R':'36', 'S':'41', 'T':'42',
+        'U':'43', 'V':'44', 'W':'45', 'X':'46',
+        'Y':'51', 'Z':'52', '0':'53', '1':'54',
+        '2':'55', '3':'56', '4':'61', '5':'62',
+        '6':'63', '7':'64', '8':'65', '9':'66'
+    }
+    if mode == 'E':
+        for symbol in message:
+            if symbol in keys:
+                final.append(keys[symbol])
+    else:
+        for twoNumbers in getNumbers(message):
+            for key in keys:
+                if twoNumbers == keys[key]:
+                    final.append(key)
+    return ".".join(final)
+
+### Ports ###
+def ports_regular(mode, text):
+    if mode == 'E': template = r"[A-Z]{2}"
+    else: template = r"[0-9]{3}"
+    return findall(template, text)
+def ports(mode, message):
+    final = []
+    stageOne = ['00'+str(x) for x in range(1,10)]
+    stageTwo = ['0'+str(x) for x in range(10,100)]
+    stageThree = [str(x) for x in range(100,676+1)]
+    N = tuple(stageOne + stageTwo + stageThree)
+    del stageOne, stageTwo, stageThree
+    coordinateX = tuple([chr(alpha) for alpha in range(65,91)])
+    coordinateY = tuple([chr(alpha) for alpha in range(65,91)])
+    cryptKeys = {x:None for x in N}
+    keys = tuple([key for key in cryptKeys])
+    counter = 0
+    for x in coordinateX:
+        for y in coordinateY:
+            cryptKeys[keys[counter]] = x + y
+            counter += 1
+    del N, coordinateX, coordinateY, counter, keys
+    message = message.upper()
+    if mode == 'E':
+        for symbol in message:
+            if symbol not in [chr(x) for x in range(65,91)]:
+                message = message.replace(symbol,'')
+        if len(message)%2 != 0: message += 'Z'
+        for symbols in ports_regular(mode, message):
+            for key in cryptKeys:
+                if symbols == cryptKeys[key]:
+                    final.append(key)
+    else:
+        for number in ports_regular(mode, message):
+            if number in cryptKeys:
+                final.append(cryptKeys[number])
+    return ".".join(final)
+
+### PowVishener ###
+def powVishener(mode, message, keys):
+    message = message.upper()
+    keys = keys.upper(); keys = keys.split()
+    for key in keys:
+        final = ""
+        key *= len(message) // len(key) + 1
+        for index, symbol in enumerate(message):
+            if mode == 'E':
+                temp = ord(symbol) + ord(key[index])
+            else:
+                temp = ord(symbol) - ord(key[index])
+            final += chr(temp % 26 + ord('A'))
+        message = final
+    return final
+
+### Psevdo ###
+def psevdo_regular(text):
+    template = r"\w{3}"
+    return findall(template, text)
+def psevdo(mode, message, final = ""):
+    keys = {
+        'A':"AAA", 'B':"AAА", 'C':"AAΑ",
+        'D':"AАA", 'E':"AАА", 'F':"AАΑ",
+        'G':"AΑA", 'H':"AΑА", 'I':"AΑΑ",
+        'J':"АAA", 'K':"АAА", 'L':"АAΑ",
+        'M':"ААA", 'N':"ААА", 'O':"ААΑ",
+        'P':"АΑA", 'Q':"АΑА", 'R':"АΑΑ",
+        'S':"ΑAA", 'T':"ΑAА", 'U':"ΑAΑ",
+        'V':"ΑАA", 'W':"ΑАА", 'X':"ΑАΑ",
+        'Y':"ΑΑA", 'Z':"ΑΑА", ' ':"ΑΑΑ"
+    }
+    if mode == 'E':
+        message = message.upper()
+        for symbol in message:
+            if symbol in keys:
+                final += keys[symbol]
+    else:
+        for threeSymbols in psevdo_regular(message):
+            for key in keys:
+                if threeSymbols == keys[key]:
+                    final += key
+    return final
+
+### Replace ###
+def replace(mode, message, final = ""):
+    message = message.upper()
+    symbolsAlpha = [chr(x) for x in range(65,91)]
+    symbolsCrypt = ('!','@','#','$','%','^','&','*','(',')','-','=',
+    '+','?',':',';','<','>','/','[',']','{','}','|','.',',','~')
+    keys = dict(zip(symbolsAlpha,symbolsCrypt))
+    if mode == 'E':
+        for symbol in message:
+            if symbol in keys: final += keys[symbol]
+    else:
+        for symbol in message:
+            for key in keys:
+                if symbol == keys[key]: final += key
+    return final
+
+### ROT13 ###
+def rot13(message):
+    message = list(message.upper())
+    for symbol in range(len(message)):
+        message[symbol] = chr(ord(message[symbol])%26+ord('A'))
+    return "".join(message)
+
+### Rotors ###
+def rotors(mode, message, final = ""):
+    message = message.upper()
+    rotors = (
+        (10,24,14,12,23,2,7,15,24,2,7,5,22,6,2,1,22,12,6,9,7,2,11,23,14,2),
+        (1,7,11,26,12,5,11,20,11,7,18,6,17,18,19,1,13,5,2,9,11,13,6,17,26,24),
+        (9,1,21,6,4,19,25,6,17,10,26,1,23,6,1,17,19,17,25,21,3,21,17,1,18,20)
+    )
+    x,y,z = 1,2,3
+    for symbol in message:
+        rotor = rotors[0][x] + rotors[1][y] + rotors[2][z]
+        if mode == 'E':
+            if symbol in [chr(x) for x in range(65,91)]:
+                final += chr((ord(symbol) - 13 + rotor)%26 + ord('A'))
+            else: continue
+        else: 
+            final += chr((ord(symbol) - 13 - rotor)%26 + ord('A'))
+        if x != 25: x += 1
+        else:
+            x = 0
+            if y != 25: y += 1
+            else:
+                y = 0
+                if z != 25: z += 1
+                else: z = 0
+    return final
+
+### RSA ###
+def rsa(mode, message, key):
+    pass
+
+### Syllables ###
+def syllables(mode, message):
+    message = message.upper()
+    syllables = ('TH','EE','OO','ING','ED','SS','DE','RE','AR',
+    'WH','AI','IS','BE','CH','SH','GH','EN','OU','LL','HE','US',
+    'ST','EV','WO','UI','IN','ER','OR','AT','RD','AL','LE','LD',
+    'UR','UP','SO','ME','SE','MY','NA','TE','NE','VE','LA','GE',
+    'ON','GU','RA','AN','AG','SH','CR','FO','OW','PY','WR','CA',
+    'EA','SP','PR','AS','AU','MA','KE','UT','DO','NT','WA','HU',
+    'AD','WI','RI','LO','FU','BR','OF','AP','TO','IF','AM','ND',
+    'LY','TA','KN','FA','TT','LP')
+    symbols = ('!!$','@@2','#99','$$!','^^$','&<<','**?',';;{',
+    '||}','::#','--/','++;','//~','==]','[++','?::','>>&','//?',
+    '&**','!<<','%((','::>','<;;','*++','?//','^$$','~""','!::',
+    '::!','&??','//!','#$$','((:',':))','{[[','[[]',';;<','|[[',
+    '$??','0//','1[[','@]]','[[<',':]]',':[[',']::','.','!','@',
+    '#','$','%','^','&','*','(',')','-','_','=','+','{','}',':',
+    ';','"',',','<','>','?','/','~','`','|','\\','[',']','1','2',
+    '3','4','5','6','7','8','9','0')
+    if mode == 'E':
+        for syllable in syllables:
+            if syllable in message:
+                message = message.replace(syllable,symbols[syllables.index(syllable)])
+    else:
+        for symbol in symbols:
+            if symbol in message:
+                message = message.replace(symbol, syllables[symbols.index(symbol)])
+    return message
+
+### Tarabar ###
+def tarabar(message):
+    keys = {
+    'B':'Z','C':'X','D':'W','F':'V','G':'T',
+    'H':'S','J':'R','K':'Q','L':'P','M':'N'}
+    message = list(message.upper())
+    for symbol in range(len(message)):
+        for key in keys:
+            if message[symbol] == key:
+                message[symbol] = keys[key]
+            elif message[symbol] == keys[key]:
+                message[symbol] = key
+            else: pass
+    return "".join(message)
+
+### Thritemius ###
+def thritemius(mode, message, key, final = ""):
+        message = list(message.upper())
+        for symbol in message:
+            if symbol not in [chr(x) for x in range(65,91)]:
+                message.remove(symbol)
+        key = eval('lambda x: ' + key) # x*2
+        for index, symbol in enumerate(message):
+            if mode == 'E':
+                temp = ord(symbol) + key(index) - 13
+            else:
+                temp = ord(symbol) - key(index) - 13
+            final += chr(temp%26 + ord('A'))
+        return final
+
+### Typex ###
+def typex_stageOne(message):
+    switch = {
+        'H':'Z', 'S':'N', 'L':'M',
+        'P':'Q', 'R':'W', 'X':'Y'}
+    message = list(message)
+    for symbol in range(len(message)):
+        for key in switch:
+            if message[symbol] == key:
+                message[symbol] = switch[key]
+            elif message[symbol] == switch[key]:
+                message[symbol] = key
+    return "".join(message)
+def typex_stageTwo(mode, message, final = ""):
+    rotors = (
+        (10,24,14,12,23,2,7,15,24,2,7,5,22,6,2,1,22,12,6,9,7,2,11,23,14,2),
+        (1,7,11,26,12,5,11,20,11,7,18,6,17,18,19,1,13,5,2,9,11,13,6,17,26,24),
+        (9,1,21,6,4,19,25,6,17,10,26,1,23,6,1,17,19,17,25,21,3,21,17,1,18,20)
+    )
+    X,Y,Z = 2,0,1; x,y,z = 1,2,3
+    for symbol in message:
+        rotor = rotors[X][x] + rotors[Y][y] + rotors[Z][z]
+        if mode == 'E':
+            if symbol in [chr(x) for x in range(65,91)]:
+                final += chr((ord(symbol) - 13 + rotor)%26 + ord('A'))
+            else: continue
+        else: 
+            final += chr((ord(symbol) - 13 - rotor)%26 + ord('A'))
+        if x != 25: x += 1
+        else:
+            x = 0
+            if y != 25: y += 1
+            else:
+                y = 0
+                if z != 25: z += 1
+                else: z = 0
+    return final
+def typex(mode, message):
+    message = message.upper()
+    if mode == 'E': 
+        message = typex_stageOne(message)
+        message = typex_stageTwo(mode, message)
+    else: 
+        message = typex_stageTwo(mode, message)
+        message = typex_stageOne(message)
+    return message
+
+### Vernam ###
+def vernam(mode, message, key, final = ""):
+    message = message.upper(); keys = []
+    if mode == 'E':
+        for symbol in message:
+            key = randint(0,25); keys.append(str(key))
+            final += chr((ord(symbol) + key - 13)%26 + ord('A'))
+        return final + '\n' + '.'.join(keys)
+    else: 
+        for index, symbol in enumerate(message):
+            final += chr((ord(symbol) - int(getNumbers(key)[index]) - 13)%26 + ord('A'))
+        return final
+
+### Vishener ###
+def vishener(mode, message, key, final = ""):
+    message = message.upper(); key = key.upper();
+    key *= len(message) // len(key) + 1
+    for index, symbol in enumerate(message):
+        if mode == 'E':
+            temp = ord(symbol) + ord(key[index])
+        else:
+            temp = ord(symbol) - ord(key[index])
+        final += chr(temp % 26 + ord('A'))
+    return final
+
+### XOR ###
+def xor(message, key):
+    message = list(message)
+    for symbol in range(len(message)):
+        try: message[symbol] = chr(ord(message[symbol]) ^ int(key))
+        except ValueError: message[symbol] = chr(ord(message[symbol]) ^ ord(key))
+    return "".join(message)
 
 class CryptographyApp(App):
 
@@ -471,12 +820,43 @@ class CryptographyApp(App):
         elif self.toggle[14].state == 'down':
             self.result.text = hill2x2(args.id, self.message.text, self.key.text)
         elif self.toggle[15].state == 'down':
-            self.result.text = ""
+            self.result.text = hill3x3(args.id, self.message.text, self.key.text)
         elif self.toggle[16].state == 'down':
             self.result.text = homophonic(args.id, self.message.text)
         elif self.toggle[17].state == 'down':
-            self.result.text = ""
-        
+            self.result.text = lattice(self.message.text)
+        elif self.toggle[18].state == 'down':
+            self.result.text = playfair(args.id, self.message.text)
+        elif self.toggle[19].state == 'down':
+            self.result.text = polibiy(args.id, self.message.text)
+        elif self.toggle[20].state == 'down':
+            self.result.text = ports(args.id, self.message.text)
+        elif self.toggle[21].state == 'down':
+            self.result.text = powVishener(args.id, self.message.text, self.key.text)
+        elif self.toggle[22].state == 'down':
+            self.result.text = psevdo(args.id, self.message.text)
+        elif self.toggle[23].state == 'down':
+            self.result.text = replace(args.id, self.message.text)
+        elif self.toggle[24].state == 'down':
+            self.result.text = rot13(self.message.text)
+        elif self.toggle[25].state == 'down':
+            self.result.text = rotors(args.id, self.message.text)
+        elif self.toggle[26].state == 'down':
+            self.result.text = rsa(args.id, self.message.text, self.key.text)
+        elif self.toggle[27].state == 'down':
+            self.result.text = syllables(args.id, self.message.text)
+        elif self.toggle[28].state == 'down':
+            self.result.text = tarabar(self.message.text)
+        elif self.toggle[29].state == 'down':
+            self.result.text = thritemius(args.id, self.message.text, self.key.text)
+        elif self.toggle[30].state == 'down':
+            self.result.text = typex(args.id, self.message.text)
+        elif self.toggle[31].state == 'down':
+            self.result.text = vernam(args.id, self.message.text, self.key.text)
+        elif self.toggle[32].state == 'down':
+            self.result.text = vishener(args.id, self.message.text, self.key.text)
+        elif self.toggle[33].state == 'down':
+            self.result.text = xor(self.message.text, self.key.text)
 
     def getInfo(self, args):
         info = {
@@ -507,22 +887,22 @@ Encrypt/Decrypt: A = Z, B = X, C = Y, ... X = 24, C = B, Z = A.", # Atbash
             '15':"", # Hill[3x3]
             '16':"", # Homophonic
             '17':"", # Lattice
-            '18':"",
-            '19':"",
-            '20':"",
-            '21':"",
-            '22':"",
-            '23':"",
-            '24':"",
-            '25':"",
-            '26':"",
-            '27':"",
-            '28':"",
-            '29':"",
-            '30':"",
-            '31':"",
-            '32':"",
-            '33':""
+            '18':"", # PlayFair
+            '19':"", # Polibiy
+            '20':"", # Ports
+            '21':"", # PowVishener
+            '22':"", # Psevdo
+            '23':"", # Replace
+            '24':"", # ROT13
+            '25':"", # Rotors
+            '26':"", # RSA
+            '27':"", # Syllables
+            '28':"", # Tarabar
+            '29':"", # Thritemius
+            '30':"", # Typex
+            '31':"", # Vernam
+            '32':"", # Vishener
+            '33':"" # XOR
         }
         self.result.text = info[args.id]
 
