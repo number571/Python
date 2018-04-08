dda8f64ebe83c064e6040e6eab3a21d10b544c78
[#571] <31502293+Number571@users.noreply.github.com>
2018-04-08 21:01:05 +0700

    Create Diffie-Hellman.py

---
+++ Cryptography/Diffie-Hellman.py
@@ -0,0 +1,26 @@
+# Public
+p = 3001 # Простое число
+g = 2    # Натуральное число
+
+if g**(p-1)%p != 1: 
+    raise SystemExit
+
+# Private
+a = 41 # Натуральное число Alice
+b = 12 # Натуральное число Bob
+# e = 31 # Натуральное число Eve
+
+# Public
+A = g**a%p # Alice
+B = g**b%p # Bob
+# E = g**e%p # Eve
+print(A, B) # , E
+
+# Private
+kA = B**a%p # Key
+kB = A**b%p # Key
+# kE = A**e%p # Key
+print(kA, kB) # , kE
+
+# Eve have:
+# p, g, A, B
