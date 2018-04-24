f19c52c642450fb0687a13e596de59e2347650aa
[#571] <31502293+Number571@users.noreply.github.com>
2018-04-24 15:46:53 +0700

    Update rsa.py

--- Cryptography/rsa.py
+++ Cryptography/rsa.py
@@ -1,102 +1,81 @@
+from random import choice
+
 # Вычисление простых чисел #
-array = []
-flag = False
-for s in range(50,125):
-	for i in range(2,s):
-		if s % i == 0:
-			flag = True
-			break
-	if flag == False:
-		array.append(s)
-	flag = False
-array.append("...")
-print("s:",array,'\n')
-
-# Простые числа
-p = 101; q = 53
-print("p = %d; q = %d"%(p,q))
-
-n = p * q # Произведение
-Fn = (p-1)*(q-1) # Функция Эйлера
-
-print("n = %d; f(n) = %d\n"%(n, Fn))
+def getPrime(minN, maxN, flag = False, primes = []):
+	for num in range(minN, maxN):
+		for get in range(2,num):
+			if num % get == 0:
+				flag = True
+				break
+		if not flag:
+			primes.append(num)
+		flag = False
+	return primes
 
 # Подбор открытой экспоненты #
-array = []
-for e in range(1,25):
-	d = int((1 + 2 * Fn) / e)
-	if d * e == 1 + 2 * Fn:
-		array.append(e)
-array.append("...")
-print("e:",array)
-
-# Открытая экспонента
-e = 3 # Простое нечётное число не имеющее общих делителей с f(n)
-print("e =", e,'\n')
+def getPubExp(minN, maxN, Fn, pubExp = []):
+	for e in range(minN, maxN):
+		d = int((1 + 2 * Fn) / e)
+		if d * e == 1 + 2 * Fn:
+			pubExp.append(e)
+	return pubExp
 
 # Подбор натурального числа k #
-array = []
-for k in range(1,25):
+def getNumK(minN, maxN, Fn, e, numK = []):
+	for k in range(minN, maxN):
+		d = int((1 + k * Fn) / e)
+		if d * e == 1 + k * Fn:
+			numK.append(k)
+	return numK
+
+# Получение приватной экспоненты #
+def getPrivExp(e, n, Fn, k):
 	d = int((1 + k * Fn) / e)
-	if d * e == 1 + k * Fn:
-		array.append(k)
-array.append("...")
-print("k:",array)
+	if d * e != 1 + k * Fn:
+		raise SystemExit
+	return d
 
-k = 2 # Натуральное число
-# Секретная экспонента
-d = int((1 + k * Fn) / e)
+# Генерация ключей #
+def generateKeys(minP, maxP, maxN):
+	primes = getPrime(minP,maxP)
+	
+	p, q = choice(primes), choice(primes)
+	if p == q: return generateKeys(minP, maxP, maxN)
 
-print("k = %d; d = %d\n"%(k,d))
+	n, Fn = p*q, (p-1)*(q-1)
+	try:
+		pubExp = getPubExp(2, maxN, Fn)[0]
+		numK = getNumK(2, maxN, Fn, pubExp)[0]
+		privExp = getPrivExp(pubExp, n, Fn, numK)
 
-# Условие на вычисление секретной экспоненты
-if d * e != 1 + k * Fn:
-	raise SystemExit
+	except: return generateKeys(minP, maxP, maxN)
 
-public_key = [e, n] # Публичный ключ
-private_key = [d, n] # Приватный ключ
+	return ([pubExp,n], [privExp,n])
 
-print("public_key:",public_key)
-print("private_key:",private_key,'\n')
+# Шифрование / Расшифрование #
+def encryptDecrypt(key, message):
+	return message ** key[0] % key[1]
 
-#'''
-# Сообщение
-m = 111
-print("m =", m)
+# Проверка выборочной опции #
+def choiceMode(mode):
+	if mode == 'G':
+		keys = generateKeys(50,500,25)
+		return \
+		"Public_key: [%d.%d]\nPrivate_key: [%d.%d]"%\
+		(keys[0][0], keys[0][1], keys[1][0], keys[1][1])
+	else:
+		try:
+			message = int(input("Write the number: "))
+			key = [int(k) for k in input("Write the key: ").split(".")]
+		except KeyboardInterrupt: 
+			print(); raise SystemExit
+		return encryptDecrypt(key, message)
 
-# Шифрование
-Cm = m ** e % n
-print("Cm =", Cm)
-
-# Расшифрование
-Dm = Cm ** d % n
-print("Dm =", Dm)
-#'''
-
-'''
-dictCodes = {chr(value+55):value for value in range(10,36)}
-message = "Hello World".upper()
-for symbol in message:
-	if symbol not in [chr(x) for x in range(65, 91)]:
-		message = message.replace(symbol,'')
-print("message =", message)
-codes = []
-for symbol in message:
-	codes.append(dictCodes[symbol])
-print("codes =", codes,'\n')
-# Шифрование
-encrypt = []
-for m in codes:
-	encrypt.append(m ** e % n)
-print("encrypt =", encrypt)
-# Расшифрование
-codes = []
-for c in encrypt:
-	codes.append(int(c) ** d % n)
-decrypt = ""
-for code in codes:
-	for key in dictCodes:
-		if code == dictCodes[key]:
-			decrypt += key
-print("decrypt =", decrypt)
-'''
+while True:
+	try:
+		cryptMode = input("[G]enerate_[C]ipher: ").upper()
+	except KeyboardInterrupt: 
+		print(); raise SystemExit
+	if cryptMode not in ['G','C']:
+		print("Error: mode is not Found!"); raise SystemExit
+	print(choiceMode(cryptMode),'\n')
