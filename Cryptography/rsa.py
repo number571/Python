565be848171b31514cd33dee3a1218b591017a9d
[#571] <31502293+Number571@users.noreply.github.com>
2018-04-26 02:58:07 +0700

    Update rsa.py

--- Cryptography/rsa.py
+++ Cryptography/rsa.py
@@ -68,7 +68,7 @@ def choiceMode(mode):
 			key = [int(k) for k in input("Write the key: ").split(".")]
 		except KeyboardInterrupt: 
 			print(); raise SystemExit
-		return encryptDecrypt(key, message)
+		return "Final message: " + str(encryptDecrypt(key, message))
 
 while True:
 	try:
