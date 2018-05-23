58c716f9ca057a666a9934d17164619d1a7534cd
[#571] <31502293+Number571@users.noreply.github.com>
2018-05-23 22:25:01 +0700

    Delete brutecaesar.py

--- Python/Cryptography/brutecaesar.py
+++
@@ -1,8 +0,0 @@
-cryptMessage = input("Write the message: ").upper()
-print("All possible variants of decrypt:")
-for key in range(26):
-	if key < 10: print("[ 0%d ]"%(key), end = ' ')
-	else: print("[ %d ]"%(key), end = ' ')
-	for symbol in cryptMessage:
-		print(chr((ord(symbol)-key-13)%26+ord('A')), end = '')
-	print()
