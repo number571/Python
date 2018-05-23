0e37762eca281dca7c360369619d1085010dc355
[#571] <31502293+Number571@users.noreply.github.com>
2018-05-23 22:26:25 +0700

    Delete en-ru.py

--- Python/Cryptography/en-ru.py
+++
@@ -1,9 +0,0 @@
-alphaEn = tuple("QWERTYUIOP[]ASDFGHJKL;'ZXCVBNM,.~")
-alphaRu = tuple("ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ")
-startMessage = input("Write the message: ").upper()
-def encryptDecrypt(message, alpha, final = ""):
-    for symbol in message:
-        try: final += alpha[1][alpha[0].index(symbol)]
-        except ValueError: final += alpha[0][alpha[1].index(symbol)]
-    return final
-print("Final message:", encryptDecrypt(startMessage, [alphaEn, alphaRu]))
