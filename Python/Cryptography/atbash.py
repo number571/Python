41ca4dcb1506ddc0952ad5634b57b339ad2d9ea7
[#571] <31502293+Number571@users.noreply.github.com>
2018-05-23 22:24:36 +0700

    Delete atbash.py

--- Python/Cryptography/atbash.py
+++
@@ -1,9 +0,0 @@
-message = input("Write the message: ").upper()
-alphaDefault = [chr(x) for x in range(65,91)]
-alphaReverse = list(alphaDefault); alphaReverse.reverse()
-final = ""
-for symbolMessage in message:
-	for indexAlpha, symbolAlpha in enumerate(alphaDefault):
-		if symbolMessage == symbolAlpha:
-			final += alphaReverse[indexAlpha]
-print("Final message:", final)
