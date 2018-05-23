748ce2f8b77cafddd69263945f42cff9eef16dd5
[#571] <31502293+Number571@users.noreply.github.com>
2018-05-23 22:28:50 +0700

    Delete rot13.py

--- Python/Cryptography/rot13.py
+++
@@ -1,4 +0,0 @@
-message = list(input("Write the message: ").upper())
-for symbol in range(len(message)):
-	message[symbol] = chr(ord(message[symbol])%26+ord('A'))
-print("Final message:", "".join(message))
