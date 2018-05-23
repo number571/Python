02b07083d00a568c79e43169246a16be7a5e4ff1
[#571] <31502293+Number571@users.noreply.github.com>
2018-05-23 22:21:14 +0700

    Delete xor.py

--- Python/Cryptography/xor.py
+++
@@ -1,6 +0,0 @@
-message = list(input("Write the message: "))
-key = input("Write the key: ")
-for symbol in range(len(message)):
-    try: message[symbol] = chr(ord(message[symbol]) ^ int(key))
-    except ValueError: message[symbol] = chr(ord(message[symbol]) ^ ord(key))
-print("Final message:","".join(message))
