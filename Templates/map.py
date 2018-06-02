aef58ee65168f598e61bcdcf30dd219fcf3cf224
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-02 14:40:34 +0700

    Create map.py

---
+++ Templates/map.py
@@ -0,0 +1,9 @@
+head = lambda x: x[0]
+tail = lambda x: x[1:len(x)]
+def mapp(func, listS, final = []):
+	if not len(listS):
+		return final
+	else:
+		final.append(func(head(listS)))
+		return mapp(func, tail(listS))
+print(mapp(lambda x: x * 2, [1,2,3,4,5]))
