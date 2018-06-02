5cec7bec2975d3b37dea081bc93b24fc211f6e79
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-02 19:43:05 +0700

    Update map.py

--- Templates/map.py
+++ Templates/map.py
@@ -1,9 +1,9 @@
 head = lambda x: x[0]
 tail = lambda x: x[1:len(x)]
-def mapp(func, listS, final = []):
+def map(func, listS, final = []):
 	if not listS:
 		return final
 	else:
 		final.append(func(head(listS)))
-		return mapp(func, tail(listS))
-print(mapp(lambda x: x * 2, [1,2,3,4,5]))
+		return map(func, tail(listS))
+print(map(lambda x: x * 2, [1,2,3,4,5]))
