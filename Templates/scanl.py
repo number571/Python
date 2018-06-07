eeca76925b6d24184de40ddaee9cd2e24c61d13c
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-08 00:34:01 +0700

    Create scanl.py

---
+++ Templates/scanl.py
@@ -0,0 +1,5 @@
+head, tail = lambda x: x[0], lambda x: x[1:len(x)]
+def scanl(func, num, List) -> list:
+    if not List: return [num]
+    else: return [num] + scanl(func, func(num, head(List)), tail(List))
+print(scanl(lambda x, y: x // y, 64, [4,2,4]))
