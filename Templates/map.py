02a0da60e1ff715f25dd69840a49d7949fd794ed
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-08 00:08:14 +0700

    Update map.py

--- Templates/map.py
+++ Templates/map.py
@@ -1,5 +1,5 @@
 head, tail = lambda x: x[0], lambda x: x[1:len(x)]
-def map(func, List, final = []):
+def map(func, List) -> list:
     if not List: return List
     else: return [func(head(List))] + map(func, tail(List))
 print(map(lambda x: x * 2, [1,2,3,4,5]))
