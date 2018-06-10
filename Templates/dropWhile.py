b7177eb179d60b3187179b668a939fa037e548ec
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-10 11:55:43 +0700

    Update dropWhile.py

--- Templates/dropWhile.py
+++ Templates/dropWhile.py
@@ -1,6 +1,6 @@
 head = lambda x: x[0]
 tail = lambda x: x[1:len(x)]
-def dropWhile(func, List):
+def dropWhile(func, List) -> list:
     if not func(head(List)): return List
     else: return dropWhile(func, tail(List))
 print(dropWhile(lambda x: x < 5, [3,4,2,1,5,6,9,10]))
