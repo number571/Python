35fabcf69611f3b64a42b4749dbd3aff5af95aa3
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-10 11:45:18 +0700

    Update foldl.py

--- Templates/foldl.py
+++ Templates/foldl.py
@@ -3,4 +3,4 @@ tail = lambda x: x[1:len(x)]
 def foldl(func, elem, List):
     if not List: return elem
     else: return foldl(func, func(elem, head(List)), tail(List))
-print(foldl(lambda x,y: x/y, 64, [4,2,4]))
+print(foldl(lambda x,y: x//y, 64, [4,2,4]))
