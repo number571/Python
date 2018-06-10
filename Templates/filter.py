e10a3ac47c750a56f7bf13c8b14a7387ff038a46
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-10 10:33:41 +0700

    Update filter.py

--- Templates/filter.py
+++ Templates/filter.py
@@ -1,7 +1,7 @@
 head = lambda x: x[0]
 tail = lambda x: x[1:len(x)]
 def filter(func, List) -> list:
-    if not List: return List
+    if not List: return []
     elif func(head(List)): return [head(List)] + filter(func, tail(List))
     else: return filter(func, tail(List))
 print(filter(lambda x: x > 2, [1,2,3,4,5]))
