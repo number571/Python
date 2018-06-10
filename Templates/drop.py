c7beaf512c81594d0db026cf33bd7284b795e356
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-10 11:55:24 +0700

    Update drop.py

--- Templates/drop.py
+++ Templates/drop.py
@@ -1,5 +1,5 @@
 tail = lambda x: x[1:len(x)]
-def drop(num, List):
+def drop(num, List) -> list:
     if not num: return List
     else: return drop(num-1,tail(List))
 print(drop(5,[1,2,3,4,5,6,7,8,9,10]))
