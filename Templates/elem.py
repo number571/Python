43650549ef2fb545b3b7921efb285b7b74b6ce38
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-10 11:41:17 +0700

    Create elem.py

---
+++ Templates/elem.py
@@ -0,0 +1,7 @@
+head = lambda x: x[0]
+tail = lambda x: x[1:len(x)]
+def elem(element, List):
+    if not List: return False
+    elif element == head(List): return True
+    else: return elem(element, tail(List))
+print(elem(3,[4,5,2,3,1]))
