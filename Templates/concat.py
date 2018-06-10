7474f413c951e355673609b40a7f0d55fe42855a
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-10 11:55:07 +0700

    Update concat.py

--- Templates/concat.py
+++ Templates/concat.py
@@ -1,6 +1,6 @@
 head = lambda x: x[0]
 tail = lambda x: x[1:len(x)]
-def concat(ListOfLists):
+def concat(ListOfLists) -> list:
     if not ListOfLists: return []
     else: return head(ListOfLists) + concat(tail(ListOfLists))
 print(concat([[1,2,3],[4,5,6],[7,8,9]]))
