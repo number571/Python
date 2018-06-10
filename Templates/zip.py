0e68e64918374a884f62a2b1fdb129101e8aea60
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-10 14:57:18 +0700

    Update zip.py

--- Templates/zip.py
+++ Templates/zip.py
@@ -1,6 +1,6 @@
 head = lambda x: x[0]
 tail = lambda x: x[1:len(x)]
-def zip(List_one, List_two):
+def zip(List_one, List_two) -> list:
     if not List_one or not List_two: return []
     else: return [(head(List_one),head(List_two))] + zip(tail(List_one), tail(List_two))
 print(zip([1,2,3],[4,5,6]))
