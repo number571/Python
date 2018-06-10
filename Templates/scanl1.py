8ebaa9d9213033828d517192f141c7acf86dc93b
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-10 14:39:43 +0700

    Create scanl1.py

---
+++ Templates/scanl1.py
@@ -0,0 +1,10 @@
+head = lambda x: x[0]
+tail = lambda x: x[1:len(x)]
+
+scnd = lambda x: head(tail(x))
+tail2 = lambda x: tail(tail(x))
+
+def scanl1(func, List):
+    if len(List) < 2: return [head(List)]
+    else: return [head(List)] + scanl1(func,[func(head(List),scnd(List))]+tail2(List))
+print(scanl1(lambda x,y: x+y, [1,2,3,4,5]))
