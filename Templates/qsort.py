39e9e66ed4991ebc64acfedbbfba91457b78cf5b
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-18 17:15:27 +0700

    Update qsort.py

--- Templates/qsort.py
+++ Templates/qsort.py
@@ -4,7 +4,7 @@ def filter(func, List) -> list:
     if not List: return []
     elif func(head(List)): return [head(List)] + filter(func, tail(List))
     else: return filter(func, tail(List))
-def qsort(List):
+def qsort(List) -> list:
     if not List: return []
     else: return qsort(filter(lambda x: x < head(List), tail(List))) + [head(List)] + qsort(filter(lambda x: x >= head(List), tail(List)))
 print(qsort([6,9,8,0,7,2,1,4,3,5]))
