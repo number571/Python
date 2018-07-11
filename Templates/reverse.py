b683e238e9d1298558ee43f315e02ca1e2cb12bc
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-12 01:36:57 +0700

    Update reverse.py

--- Templates/reverse.py
+++ Templates/reverse.py
@@ -1,6 +1,15 @@
+head = lambda x: x[0]
+tail = lambda x: x[1:]
+def reverse(List) -> list:
+    if not List: return []
+    else: return reverse(tail(List)) + [head(List)]
+print(reverse([1,2,3,4,5]))
+
+'''
 last = lambda x: x[-1]
 init = lambda x: x[:-1]
 def reverse(List) -> list:
     if not List: return []
     else: return [last(List)] + reverse(init(List))
 print(reverse([1,2,3,4,5]))
+'''
