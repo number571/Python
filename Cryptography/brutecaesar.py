56d179432a1aae21bcb68bd941ea51db377b2dd3
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-20 21:35:38 +0700

    Update brutecaesar.py

--- Cryptography/brutecaesar.py
+++ Cryptography/brutecaesar.py
@@ -4,5 +4,10 @@ for key in range(26):
 	if key < 10: print("[ 0%d ]"%(key), end = ' ')
 	else: print("[ %d ]"%(key), end = ' ')
 	for symbol in cryptMessage:
-		print(chr((ord(symbol)-key-13)%26+ord('A')), end = '')
+		if symbol in [chr(x) for x in range(48,58)] \
+		or symbol in [chr(x) for x in range(65,91)] \
+		or symbol in [chr(x) for x in range(97,123)]:
+			print(chr((ord(symbol)-key-13)%26+ord('A')), end = '')
+		else:
+			print(symbol, end = '')
 	print()
