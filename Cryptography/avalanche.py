6ebe7d37ddc05b8221481ae135365345fa236502
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-20 23:58:29 +0700

    Update avalanche.py

--- Cryptography/avalanche.py
+++ Cryptography/avalanche.py
@@ -1,20 +1,5 @@
 # Cipher created from: https://github.com/pravdakotchestno
 
-m = 32; n = 127; length = n - m
-dictN = {x-m+1 : chr(x) for x in range(m,n)}
-dictC = {chr(x) : x-m+1 for x in range(m,n)}
-
-cryptMode = input("[E]ncrypt|[D]ecrypt: ").upper()
-if cryptMode not in ['E','D']:
-    print("Error: mode is not found")
-    raise SystemExit
-
-startMessage = input("Write the message: ")
-mainKey = int(input("Write the key: "))
-for symbol in startMessage:
-    if symbol not in [chr(x) for x in range(m,n)]:
-        startMessage = startMessage.replace(symbol, '')
-
 def encrypt(message, key, result = ""):
     for index, symbol in enumerate(message):
         pos = len(message) - index
@@ -36,6 +21,21 @@ def sumAll(message, summ = 0):
 def toString(listS, final = ""):
     for symbol in listS: final += symbol
     return final
+
+m = 32; n = 127; length = n - m
+dictN = {x-m+1 : chr(x) for x in range(m,n)}
+dictC = {chr(x) : x-m+1 for x in range(m,n)}
+
+cryptMode = input("[E]ncrypt|[D]ecrypt: ").upper()
+if cryptMode not in ['E','D']:
+    print("Error: mode is not found")
+    raise SystemExit
+
+startMessage = input("Write the message: ")
+mainKey = int(input("Write the vector: "))
+for symbol in startMessage:
+    if symbol not in [chr(x) for x in range(m,n)]:
+        startMessage = startMessage.replace(symbol, '')
 
 def encryptDecrypt(mode, message, key):
     if mode == 'E':
